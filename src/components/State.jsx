153e66ac2fb807e12f49714402d57b6396218abc
appsDev <bilalhussian785@gmail.com>
2022-08-19 12:21:37 +0500

    modified app

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -3,21 +3,32 @@ import { useState,React} from 'react'
 function State() {
 
   const [count, setCount] = useState(0);
+  const [changeCount, setChangeCount] = useState('Invalid Value');
 
 
   //.. Increment counter
   function updateCounter() {
-    setCount(count + 1);
+   setCount((nextVal) => {
+        return nextVal = nextVal + 1;
+   })
   };
 
   function decCounter() {
-    setCount(count - 1);
+    setCount((preVal) => {
+        return preVal = preVal - 1;
+    })
   };
+
+
+ 
+
+
+  
   return (
    
     <div className='counter'>
         <button id='btn-counter' onClick={updateCounter}>+</button>
-        <h2 className='output'>{count}</h2>
+        <h2 className='output'>{count < 0? changeCount : count}</h2>
         <button id="btn-counter" onClick={decCounter}>-</button>
     </div>
   )
