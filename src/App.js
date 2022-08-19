67b224cffedb4634708d60b71c97cf5f75ca3cb1
appsDev <bilalhussian785@gmail.com>
2022-08-19 16:20:01 +0500

    modified app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 import Header from './components/Header';
 import Meme from './components/Meme';
+import State from './components/State';
 
 function App() {
   return (
     <div className="App">
        <Header></Header>
        <Meme></Meme>
+
+       <div className="test-stats">
+         <State></State>
+       </div>
     </div>
   );
 }
