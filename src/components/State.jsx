388c3a487b54ccb1ccacd553904fdad9fabb40dc
appsDev <bilalhussian785@gmail.com>
2022-08-19 12:10:08 +0500

    modified app

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState,React} from 'react'
 
 function State() {
+
+  const [count, setCount] = useState(0);
+
+
+  //.. Increment counter
+  function updateCounter() {
+    setCount(count + 1);
+  };
+
+  function decCounter() {
+    setCount(count - 1);
+  };
   return (
-    <div>State</div>
+   
+    <div className='counter'>
+        <button id='btn-counter' onClick={updateCounter}>+</button>
+        <h2 className='output'>{count}</h2>
+        <button id="btn-counter" onClick={decCounter}>-</button>
+    </div>
   )
 }
 
